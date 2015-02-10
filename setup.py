0ab70d781476a538a2046443954008277bee5c2f
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-02-10 10:04:09 +0100

    Catch to tasks found and bump version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='frigg-runner',
-    version='0.0.3',
+    version='0.0.4',
     description="Frigg runner execute .frigg.yml file localy.",
     long_description=readme,
     author="Eirik Martiniussen Sylliaas",
