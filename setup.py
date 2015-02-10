7c62b06cd874fa8565c754a8dcc3aad4fec5c66e
Rolf Erik Lekang <me@rolflekang.com>
2015-02-10 12:32:41 +0100

    Remove reference to distutils

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 from frigg_runner import __version__
 
